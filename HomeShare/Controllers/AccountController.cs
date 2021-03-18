62f812fac97ba44c04910201bb176fdf02401b4b
Grafé <Sophie>
2021-03-19 00:43:21 +0100

    ajout login httppost + logout

--- HomeShare/Controllers/AccountController.cs
+++ HomeShare/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using HomeShare.Models;
+﻿using HomeShare.infra;
+using HomeShare.Models;
 using HomeShare.Repositories;
 using System;
 using System.Collections.Generic;
@@ -25,8 +26,7 @@ namespace HomeShare.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Register(MembreModel mm)
-        {            
-            
+        {
             if (ModelState.IsValid)
             {
                 UnitOfWork uow = new UnitOfWork(ConfigurationManager.ConnectionStrings["Cnstr"].ConnectionString);
@@ -49,9 +49,45 @@ namespace HomeShare.Controllers
             }
         }
 
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+
+        /***Recuperation du Login de l'utilisateur enregistré ***/
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel lm)
+        {
+            if (ModelState.IsValid)
+            {
+                if (lm.Login != "Chou" && lm.Password != "test1234")
+                {
+                    ViewBag.Error = "Votre login ou votre mot de passe ne correspondent pas, veuillez réessayer.";
+                    return View();
+                }
+                else
+                {
+                    SessionUtils.IsLogged = true;
+                    return RedirectToAction("Index", "Home", new { area = "Membre" });
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        /*------- OUT ------*/
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
         }
 
 
