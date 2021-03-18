a76e230cd909b232fdd04092acc18919ca555e0b
Grafé <Sophie>
2021-03-19 00:44:57 +0100

    création de la classe static sessionUtils + variable de session isLogged

---
+++ HomeShare/infra/SessionUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeShare.infra
+{
+    public class SessionUtils
+    {
+        public static bool IsLogged
+        {
+            get
+            {
+                if (HttpContext.Current.Session["IsLogged"] == null)
+                {
+                    HttpContext.Current.Session["IsLogged"] = false;
+                }
+                return (bool)HttpContext.Current.Session["IsLogged"];
+            }
+            set
+            {
+                HttpContext.Current.Session["IsLogged"] = value;
+            }
+        }
+    }
+}
