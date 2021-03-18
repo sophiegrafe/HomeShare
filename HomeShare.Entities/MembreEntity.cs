1799bc9c3a3d6453c5f21d03a1cbc333be8d53c3
Grafé <Sophie>
2021-03-18 22:28:57 +0100

    Création MembreEntity

---
+++ HomeShare.Entities/MembreEntity.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeShare.Entities
+{
+    public class MembreEntity
+    {
+        #region Fields
+        private int _idMembre;
+        private string _nom, _prenom, _email, _pays, _telephone, _login, _password;
+        #endregion
+
+        #region Properties
+        public int IdMembre
+        {
+            get
+            {
+                return _idMembre;
+            }
+
+            set
+            {
+                _idMembre = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return _prenom;
+            }
+
+            set
+            {
+                _prenom = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public string Pays
+        {
+            get
+            {
+                return _pays;
+            }
+
+            set
+            {
+                _pays = value;
+            }
+        }
+
+        public string Telephone
+        {
+            get
+            {
+                return _telephone;
+            }
+
+            set
+            {
+                _telephone = value;
+            }
+        }
+
+        public string Login
+        {
+            get
+            {
+                return _login;
+            }
+
+            set
+            {
+                _login = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+            }
+        } 
+        #endregion
+    }
+}
