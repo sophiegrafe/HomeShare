2ce883fb12da0f4246322939ec814cd072c3e6bb
Grafé <Sophie>
2021-03-19 00:43:53 +0100

    ajout des methodes session start /end

--- HomeShare/Global.asax.cs
+++ HomeShare/Global.asax.cs
@@ -17,5 +17,20 @@ namespace HomeShare
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_End()
+        {
+
+        }
+
+        protected void Session_Start()
+        {
+
+        }
+
+        protected void Session_End()
+        {
+
+        }
     }
 }
