3a184d6976680b8006fa58761c0921f98800239f
Grafé <Sophie>
2021-03-18 10:48:05 +0100

    modification et ajout bundles template

--- HomeShare/App_Start/BundleConfig.cs
+++ HomeShare/App_Start/BundleConfig.cs
@@ -16,15 +16,46 @@ namespace HomeShare
 
             // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
             // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
+            // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/css/style.css"));
+
+            bundles.Add(new StyleBundle("~/Content/owlCarousel").Include(
+                      "~/css/owl.carousel.css",
+                      "~/css/owl.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/slitSlider").Include(
+                      "~/css/slitSlider-custom.css",
+                      "~/css/slitSlider-style.css"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/slitSlider").Include(
+                        "~/js/jquery.slitslider.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/owlCarousel").Include(
+                        "~/js/owl.carousel.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/customScript").Include(
+                        "~/js/script.js"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+        
+    
         }
     }
 }
