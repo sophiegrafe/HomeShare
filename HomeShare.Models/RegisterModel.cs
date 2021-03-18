27e95d2375b2426abaf98a83fea2fbf049cfe560
Grafé <Sophie>
2021-03-18 15:53:01 +0100

    modf à ignorer

---
+++ HomeShare.Models/RegisterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeShare.Models
+{
+    class MembreModel
+    {
+        private int idMembre;
+        private string _nom, _prenom, _email, _pays, _telephone, _login, _password;
+    }
+}
