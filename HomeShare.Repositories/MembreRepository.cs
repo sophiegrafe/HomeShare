2ec2abd4e9a0bb17fef08284b3b54adaefe2b996
Grafé <Sophie>
2021-03-18 22:27:34 +0100

    Insérer un membre -->  appel ps_Membre_Insert

---
+++ HomeShare.Repositories/MembreRepository.cs
@@ -0,0 +1,55 @@
+﻿using HomeShare.DAL.Repositories;
+using HomeShare.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeShare.Repositories
+{
+    public class MembreRepository : BaseRepository<MembreEntity>, IConcreteRepository<MembreEntity>
+    {
+        #region Ctor
+        public MembreRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public bool Delete(MembreEntity toDelete)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Methodes
+        public List<MembreEntity> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MembreEntity GetOne(int PK)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(MembreEntity toInsert)
+        {
+            string requete = @"EXECUTE [dbo].[sp_Membre_Insert] 
+            @login
+            ,@password
+            ,@nom
+            ,@prenom
+            ,@email
+            ,@telephone
+            ,@pays";
+
+            return base.Insert(toInsert, requete);
+        }
+
+        public bool Update(MembreEntity toUpdate)
+        {
+            throw new NotImplementedException();
+        } 
+        #endregion
+    }
+}
