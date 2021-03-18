ea315591bae143feb7f8b0574f500dc9bec020ff
Grafé <Sophie>
2021-03-18 22:28:26 +0100

    Inscription d'un membre mapping pour CreateMembre

--- HomeShare.Repositories/UnitOfWork.cs
+++ HomeShare.Repositories/UnitOfWork.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HomeShare.Entities;
+using HomeShare.Models;
+using HomeShare.DAL.Repositories;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,33 @@ using System.Threading.Tasks;
 
 namespace HomeShare.Repositories
 {
-    class UnitOfWork
+    public class UnitOfWork
     {
+        IConcreteRepository<MembreEntity> _membreRepo;
+        public UnitOfWork(string connectionString)
+        {
+            _membreRepo = new MembreRepository(connectionString);
+        }
+
+        #region Account
+        public bool CreateMember(MembreModel mm)
+        {
+            MembreEntity membreEntity = new MembreEntity()
+            {
+                Nom = mm.Nom,
+                Prenom = mm.Prenom,
+                Login = mm.Login,
+                Password = mm.Password,
+                Email = mm.Email,
+                Telephone = mm.Telephone,
+                Pays = mm.Pays,
+            };
+            return _membreRepo.Insert(membreEntity);
+
+
+        }
+
+        #endregion
+
     }
 }
