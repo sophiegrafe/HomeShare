b0f3fd4c79c7374f36ba9a19f857c1c4e5d6e3d4
Grafé <Sophie>
2021-04-04 21:54:29 +0200

    ajout controllers area membre pour préparer update infos profil et ajout bien

---
+++ HomeShare/Areas/Membre/Controllers/ProfilController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeShare.Models;
+
+namespace HomeShare.Areas.Membre.Controllers
+{
+    public class ProfilController : Controller
+    {
+        // GET: Membre/Profil
+        public ActionResult MyInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyInfo(MembreModel mm)
+        {   
+
+            return View();
+        }
+    }
+}
---
+++ HomeShare/Areas/Membre/Controllers/SharingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeShare.Areas.Membre.Controllers
+{
+    public class SharingController : Controller
+    {
+        // GET: Membre/Sharing
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
